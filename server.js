0331b779f12e024a87f6c6127cfbd16b8df4cb13
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-08-10 15:18:02 -0600

    Broke the .html into two sections; got the second (no form) working

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 ﻿// jquery-express-file-upload/server.js
 
+// TODO: Use npm package multer ? https://www.npmjs.com/package/multer
+// "Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files. It is written on top of busboy for maximum efficiency.
+// NOTE: Multer will not process any form which is not multipart (multipart/form-data)."
+
 require('rootpath')();
 
-//var bodyParser = require('body-parser');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
@@ -11,25 +14,28 @@ const config = require('./config');			// I.e. ./config.json
 
 const app = express();
 
-const serverListenPort = config.listenPort;	// 3000;
+const serverListenPort = config.listenPort;	// || 3000;
  
 app.use(fileUpload());
-// app.use(bodyParser.text());	// Text is the default body-parser anyways.
 
 app.get('/', function (req, res) {
 	console.log('GET /');
 	res.sendFile(__dirname + '/index.html'); // TW TODO: Use path.join
 });
 
-app.post('/', function(req, res) {
-	console.log('POST /');
+app.post('/form', function(req, res) {
+	console.log('POST /form');
+	res.send('POST /form');
+});
+
+app.post('/noform', function(req, res) {
+	console.log('POST /noform');
 	console.log('req.files is', req.files);
-	//res.send('Response to POST to / : OK');
 
 	if (req.files) {
 		// TODO: If the directory __dirname + '/uploads does not exist, create it.
-		var destPath = __dirname + '/uploads/' + req.files.file.name;
-		fs.writeFile(destPath, req.files.file.data, function (errorWriteFile) {
+		var destPath = __dirname + '/uploads/' + req.files.fileNoForm.name;
+		fs.writeFile(destPath, req.files.fileNoForm.data, function (errorWriteFile) {
 			
 			if (errorWriteFile) {
 				console.error('Error while saving uploaded file:', error);
@@ -43,6 +49,11 @@ app.post('/', function(req, res) {
 		console.error('req.files is empty.');
 		res.status(500).send('req.files is empty.');
 	}
+});
+
+app.get('/jquery.min.js', function (req, res) {
+	// res.redirect('https://code.jquery.com/jquery-3.2.1.min.js');
+	res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
 });
 
 // Start the server:
